e2aef1f770752b1b25616a7024e44436d7cf66ec
joshmotley <joshkmotley@gmail.com>
2015-09-17 15:08:25 -0400

    functions properly according to directions

---
+++ HellaCells/HellaCells/FISMasterTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISMasterTableViewController.h
+//  HellaCells
+//
+//  Created by Joshua Motley on 9/17/15.
+//  Copyright (c) 2015 Motley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FISDetailViewController.h"
+
+@interface FISMasterTableViewController : UITableViewController
+@property (strong, nonatomic) NSMutableArray *numArray;
+
+@end
