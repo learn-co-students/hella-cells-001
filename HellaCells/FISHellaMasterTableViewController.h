5b572f2dbc5c26528aff8854f0643010623b07cb
kenmhaggerty <kenmhaggerty@gmail.com>
2016-02-04 17:19:11 -0500

    Completes exercise

---
+++ HellaCells/FISHellaMasterTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISHellaMasterTableViewController.h
+//  HellaCells
+//
+//  Created by Ken M. Haggerty on 2/4/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISHellaMasterTableViewController : UITableViewController
+
+@end
