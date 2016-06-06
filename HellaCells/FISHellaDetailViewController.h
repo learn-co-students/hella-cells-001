50566dd65ad55813ad8713897783fcb459c7f6da
John Richardson <richardsonjm@gmail.com>
2016-06-06 10:23:26 -0400

    Create detail view controller

---
+++ HellaCells/FISHellaDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  FISHellaDetailViewController.h
+//  HellaCells
+//
+//  Created by John Richardson on 6/6/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISHellaDetailViewController : UIViewController
+
+@end
---
+++ HellaCells/FISHellaDetailViewController.m
@@ -0,0 +1,13 @@
+//
+//  FISHellaDetailViewController.m
+//  HellaCells
+//
+//  Created by John Richardson on 6/6/16.
+//  Copyright © 2016 FIS. All rights reserved.
+//
+
+#import "FISHellaDetailViewController.h"
+
+@implementation FISHellaDetailViewController
+
+@end
